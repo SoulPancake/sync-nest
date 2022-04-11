33f4060d950c80773c6993c2e3a4fddf383e6bf1
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-04-11 14:50:46 +0530

    resume from Avatar

---
+++ src/components/Client.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Client=()=>{
+    return (
+    <div className="client">
+        <Avatar></Avatar>
+         <span className="userName">{username}</span>
+   </div>)
+}
--- src/pages/CodePage.js
+++ src/pages/CodePage.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CodePage=()=>{
-    return <div>CodePage</div>
+   const [clients,setClients]=useState([
+       {socketId:1,username:'Anurag Bee'},
+       {socketId:2,username:'Guthrie Govan'}
+    ]);
+
+
+
+    return <div className="mainWrap">
+<div className="aside">
+    <div className="asideInner">
+        <div className="asideLogo">
+            <img src="/SyncNest.png" alt="Sync Nest Logo" className="codePageLogo"></img>
+        </div>
+        <h3>Connected</h3>
+        <div className="clientsList">
+          {
+              clients.map(client=>(
+                  <Client key={client.socketId} username={client.username}/>
+              ))
+          }
+        </div>
+    </div>
+</div>
+<div className="editorWrap">
+    Editor goes here..
+</div>
+    </div>
 };
 
 export default CodePage;
