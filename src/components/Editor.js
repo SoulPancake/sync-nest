3d7dae5ad258a614fdbff73b087889b9eef383b4
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-04-12 14:00:40 +0530

    Editor page styling completed

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,32 @@
 
-import React from "react"
-
+import React, { useEffect } from "react"
+import Codemirror from 'codemirror';
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/theme/yonce.css';
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
  const Editor=()=>{
-    return <div>Editor goes here</div>
+     useEffect(()=>{
+       async function init(){
+         Codemirror.fromTextArea(document.getElementById('realtimeEditor'),
+         {
+             
+            mode:{name:'javascript',json:true},
+             theme:'yonce',
+             autoCloseTags:true,
+             autoCloseBrackets:true,
+             lineNumbers:true,
+             indentUnit:5,
+             indentWithTabs:true,
+      
+         },);
+       }   
+       init();
+     },[]);
+
+
+    return <textarea className="editor" id="realtimeEditor"></textarea>;
     
 }
 
