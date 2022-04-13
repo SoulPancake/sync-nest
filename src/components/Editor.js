e05774e78837eecaf4b60371d1a48224fd7b5dbf
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-04-13 18:06:29 +0530

    fixed Code page UI

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,28 +7,25 @@ import 'codemirror/theme/yonce.css';
 import 'codemirror/addon/edit/closetag'
 import 'codemirror/addon/edit/closebrackets'
  const Editor=()=>{
-     useEffect(()=>{
-       async function init(){
-         Codemirror.fromTextArea(document.getElementById('realtimeEditor'),
-         {
-             
-            mode:{name:'javascript',json:true},
-             theme:'yonce',
-             autoCloseTags:true,
-             autoCloseBrackets:true,
-             lineNumbers:true,
-             indentUnit:5,
-             indentWithTabs:true,
-             
-      
-         },);
-           
-       }   
-       init();
-     },[]);
+    
+    useEffect(()=>{
+        async function init(){
+            Codemirror.fromTextArea(document.getElementById('realtimeeditor'),
+            {
+                mode:{name:'javascript',json:true},
+                theme:'yonce',
+                autoCloseTags:true,
+                autoCloseBrackets:true,
+                lineNumbers:true,
+            },{
+                
+            })
+        };
+        init();
+    },[]);
 
 
-    return <textarea id="realtimeEditor"></textarea>;
+    return <textarea id='realtimeeditor'></textarea>;
     
 }
 
