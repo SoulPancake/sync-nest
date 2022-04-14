42fd7531c262b195f3510c7a855c7a5396a64141
Anuragkillswitch <70265851+Anuragkillswitch@users.noreply.github.com>
2022-04-14 22:46:21 +0530

    commit kar

--- src/components/Client.js
+++ src/components/Client.js
@@ -3,7 +3,7 @@ import Animal from "react-animals";
 const Client=({username})=>{
     return (
     <div className="client">
-        <Animal dance size="100%" className="avatar"></Animal>
+        <Animal dance className="avatar"></Animal>
         
         <span className="username">{username}</span>
          
